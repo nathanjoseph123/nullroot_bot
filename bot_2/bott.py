450a1cc72b036f46fab8a773dd9bc264b62e344d
Nathan joseph <152595802+nathanjoseph123@users.noreply.github.com>
2026-07-05 13:57:38 +0100

    Update bott.py

--- bot_2/bott.py
+++ bot_2/bott.py
@@ -24,7 +24,7 @@ def on_start(mode, server_input, auth_input):
     if st.session_state._bot_instance["bot"] is not None:
         try:
             st.session_state._bot_instance["bot"].scrap.site.quit()
-            st.session_state._bot_instance["bot"].ben=False
+            
         except Exception:
             pass
     st.session_state._bot_instance["bot"] = bot(server_input, auth_input)
